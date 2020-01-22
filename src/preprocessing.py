c8f75c6c413fa61202427801236625c6ef0b00d3
Alberto <a_baldrati@hotmail.it>
2020-01-22 12:26:07 +0100

    fix(preprocessing): fix list slice warning

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -13,8 +13,8 @@ def crop_images(input_path, output_path):
             img = cv2.resize(img, (1042, 1464))
 
             # Change if the format of the page is different
-            mask_even_page = [slice(100, 1205), slice(120, 900)]
-            mask_odd_page = [slice(95, 1195), slice(180, 970)]
+            mask_even_page = (slice(100, 1205), slice(120, 900))
+            mask_odd_page = (slice(95, 1195), slice(180, 970))
 
             crop_img = img[mask_even_page] if (int(image[-5]) % 2 == 0) else img[mask_odd_page]
             cv2.imwrite(os.path.join(output_path, f"{image[:-4]}.png"), crop_img)
