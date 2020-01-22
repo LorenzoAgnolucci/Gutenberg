471fa4a85616cc72094ee410f1757d7eba58044a
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-01-22 15:13:11 +0100

    feat(caput): add basic caput detection routines

---
+++ src/caput.py
@@ -0,0 +1,44 @@
+import os
+
+import cv2
+import numpy as np
+
+
+def find_caput_pixels(page_image_path):
+    img = cv2.imread(page_image_path)
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower_red = np.array([0, 150, 100])
+    upper_red = np.array([15, 255, 255])
+
+    lower_blue = np.array([90, 100, 70])
+    upper_blue = np.array([140, 200, 150])
+
+    red_mask = cv2.inRange(hsv_img, lower_red, upper_red)
+    blue_mask = cv2.inRange(hsv_img, lower_blue, upper_blue)
+
+    red_channel = cv2.bitwise_and(img, img, mask=red_mask)
+    blue_channel = cv2.bitwise_and(img, img, mask=blue_mask)
+
+    _, red_binarized_channel = cv2.threshold(red_channel, 127, 255, cv2.THRESH_BINARY)
+    _, blue_binarized_channel = cv2.threshold(blue_channel, 127, 255, cv2.THRESH_BINARY)
+
+    return red_binarized_channel, blue_binarized_channel
+
+
+
+
+def main():
+    input_path = "../dataset/deskewed/exodus"
+
+    for file_name in sorted(os.listdir(input_path)):
+        red_ch, blue_ch = find_caput_pixels(os.path.join(input_path, file_name))
+        cv2.imshow(f"{file_name} blue", blue_ch)
+        cv2.waitKey(0)
+        cv2.imshow(f"{file_name} red", red_ch)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
