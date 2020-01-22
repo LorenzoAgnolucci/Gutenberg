6d3b07e05ae9a3e9301d8280f4b34915c8a01662
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-01-22 21:02:35 +0100

    refactor(preprocessing): change variable names to be more in line with rest of codebase

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,28 +7,28 @@ from scipy.ndimage import interpolation
 
 
 def crop_images(input_path, output_path):
-    for image in os.listdir(input_path):
-        if image.endswith('.jpg') or image.endswith('.png'):
-            img = cv2.imread(os.path.join(input_path, image))
-            img = cv2.resize(img, (1042, 1464))
+    for file_name in os.listdir(input_path):
+        if file_name.endswith('.jpg') or file_name.endswith('.png'):
+            image = cv2.imread(os.path.join(input_path, file_name))
+            image = cv2.resize(image, (1042, 1464))
 
             # Change if the format of the page is different
             mask_even_page = (slice(100, 1205), slice(120, 900))
             mask_odd_page = (slice(95, 1195), slice(180, 970))
 
-            crop_img = img[mask_even_page] if (int(image[-5]) % 2 == 0) else img[mask_odd_page]
-            cv2.imwrite(os.path.join(output_path, f"{image[:-4]}.png"), crop_img)
+            crop_image = image[mask_even_page] if (int(file_name[-5]) % 2 == 0) else image[mask_odd_page]
+            cv2.imwrite(os.path.join(output_path, f"{file_name[:-4]}.png"), crop_image)
 
 
 def deskew_images(input_path, output_path):
     for file_name in sorted(os.listdir(input_path)):
         if file_name.endswith('.jpg') or file_name.endswith('.png'):
-            img = im.open(os.path.join(input_path, file_name))
-            name_img = os.path.basename(file_name)[:-4]
+            image = im.open(os.path.join(input_path, file_name))
+            output_file = os.path.basename(file_name)[:-4]
 
             # convert to binary
-            width, height = img.size
-            pix = np.array(img.convert('1').getdata(), np.uint8)
+            width, height = image.size
+            pix = np.array(image.convert('1').getdata(), np.uint8)
             bin_img = 1 - (pix.reshape((height, width)) / 255.0)
 
             delta = 1
@@ -44,9 +44,9 @@ def deskew_images(input_path, output_path):
             print(f'Best angle: {best_angle} {file_name}')
 
             # correct skew
-            data = interpolation.rotate(img, best_angle, reshape=False, order=0, mode='nearest')
-            img = im.fromarray((data).astype("uint8")).convert("RGB")
-            img.save(os.path.join(os.path.abspath(output_path), f"{name_img}.png"))
+            data = interpolation.rotate(image, best_angle, reshape=False, order=0, mode='nearest')
+            image = im.fromarray(data.astype("uint8")).convert("RGB")
+            image.save(os.path.join(os.path.abspath(output_path), f"{output_file}.png"))
 
 
 def find_score(arr, angle):
