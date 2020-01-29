af00652936c8910bc8aaac860d75b7bb572ce356
Alberto <a_baldrati@hotmail.it>
2020-01-29 18:05:34 +0100

    refactor(punctuation): rename all constant and variables to improve consistency

--- src/punctuation.py
+++ src/punctuation.py
@@ -38,20 +38,18 @@ def find_periods_in_line(page_image, line_left, line_top, line_right, line_botto
             if PERIOD_BOTTOM_BOUNDINGBOX_LOWER_BOUND < bottom < PERIOD_BOTTOM_BOUNDINGBOX_UPPER_BOUND \
                     and top > PERIOD_TOP_BOUNDINGBOX_LOWER_BOUND \
                     and columns_black_pixels_sum < BLACK_PIXELS_PERIOD_HISTOGRAM_THRESHOLD:
-
                 periods_in_line.append((line_left + left, line_top + top, line_left + right, line_top + bottom))
 
-    # cv2.imwrite(output_path_image, page_image)
     return periods_in_line
 
 
 def find_middle_periods_in_line(page_image, line_left, line_top, line_right, line_bottom):
-    PERIOD_AREA_LOWER_BOUND = 7
-    PERIOD_AREA_UPPER_BOUND = 35
-    PERIOD_BOTTOM_BOUNDINGBOX_LOWER_BOUND = 13
-    PERIOD_BOTTOM_BOUNDINGBOX_UPPER_BOUND = 18
-    PERIOD_TOP_BOUNDINGBOX_LOWER_BOUND = 10
-    BLACK_PIXELS_PERIOD_HISTOGRAM_THRESHOLD = 8
+    MIDDLE_PERIOD_AREA_LOWER_BOUND = 7
+    MIDDLE_PERIOD_AREA_UPPER_BOUND = 35
+    MIDDLE_PERIOD_BOTTOM_BOUNDINGBOX_LOWER_BOUND = 13
+    MIDDLE_PERIOD_BOTTOM_BOUNDINGBOX_UPPER_BOUND = 18
+    MIDDLE_PERIOD_TOP_BOUNDINGBOX_LOWER_BOUND = 10
+    BLACK_PIXELS_MIDDLE_PERIOD_HISTOGRAM_THRESHOLD = 8
 
     middle_periods_in_line = []
 
@@ -67,30 +65,28 @@ def find_middle_periods_in_line(page_image, line_left, line_top, line_right, lin
         right = left + stats[label, cv2.CC_STAT_WIDTH]
 
         area = (bottom - top) * (right - left)
-        period_columns_histogram = cv2.reduce(line_image[:, left:right], 0, cv2.REDUCE_SUM, dtype=cv2.CV_32F).reshape(
+        middle_period_columns_histogram = cv2.reduce(line_image[:, left:right], 0, cv2.REDUCE_SUM, dtype=cv2.CV_32F).reshape(
             -1) / 255
-        period_black_pixels = stats[label, cv2.CC_STAT_AREA]
+        middle_period_black_pixels = stats[label, cv2.CC_STAT_AREA]
 
-        columns_black_pixels_sum = sum(period_columns_histogram) - period_black_pixels
+        columns_black_pixels_sum = sum(middle_period_columns_histogram) - middle_period_black_pixels
 
-        if PERIOD_AREA_LOWER_BOUND < area < PERIOD_AREA_UPPER_BOUND:
-            if PERIOD_BOTTOM_BOUNDINGBOX_LOWER_BOUND < bottom < PERIOD_BOTTOM_BOUNDINGBOX_UPPER_BOUND \
-                    and top > PERIOD_TOP_BOUNDINGBOX_LOWER_BOUND \
-                    and columns_black_pixels_sum < BLACK_PIXELS_PERIOD_HISTOGRAM_THRESHOLD:
-
+        if MIDDLE_PERIOD_AREA_LOWER_BOUND < area < MIDDLE_PERIOD_AREA_UPPER_BOUND:
+            if MIDDLE_PERIOD_BOTTOM_BOUNDINGBOX_LOWER_BOUND < bottom < MIDDLE_PERIOD_BOTTOM_BOUNDINGBOX_UPPER_BOUND \
+                    and top > MIDDLE_PERIOD_TOP_BOUNDINGBOX_LOWER_BOUND \
+                    and columns_black_pixels_sum < BLACK_PIXELS_MIDDLE_PERIOD_HISTOGRAM_THRESHOLD:
                 middle_periods_in_line.append((line_left + left, line_top + top, line_left + right, line_top + bottom))
 
-    # cv2.imwrite(output_path_image, page_image)
     return middle_periods_in_line
 
 
 def find_colons_in_line(page_image, line_left, line_top, line_right, line_bottom):
-    PERIOD_AREA_LOWER_BOUND = 7
-    PERIOD_AREA_UPPER_BOUND = 35
-    PERIOD_BOTTOM_BOUNDINGBOX_LOWER_BOUND = 18
-    PERIOD_BOTTOM_BOUNDINGBOX_UPPER_BOUND = 23
-    PERIOD_TOP_BOUNDINGBOX_LOWER_BOUND = 14
-    BLACK_PIXELS_PERIOD_HISTOGRAM_THRESHOLD = 8
+    COLON_AREA_LOWER_BOUND = 7
+    COLON_AREA_UPPER_BOUND = 35
+    COLON_BOTTOM_BOUNDINGBOX_LOWER_BOUND = 18
+    COLON_BOTTOM_BOUNDINGBOX_UPPER_BOUND = 23
+    COLON_TOP_BOUNDINGBOX_LOWER_BOUND = 14
+    BLACK_PIXELS_COLON_HISTOGRAM_THRESHOLD = 8
 
     colons_in_line = []
 
@@ -106,36 +102,34 @@ def find_colons_in_line(page_image, line_left, line_top, line_right, line_bottom
         right = left + stats[label, cv2.CC_STAT_WIDTH]
 
         area = (bottom - top) * (right - left)
-        period_columns_histogram = cv2.reduce(line_image[:, left:right], 0, cv2.REDUCE_SUM, dtype=cv2.CV_32F).reshape(
+        colon_columns_histogram = cv2.reduce(line_image[:, left:right], 0, cv2.REDUCE_SUM, dtype=cv2.CV_32F).reshape(
             -1) / 255
-        period_black_pixels = stats[label, cv2.CC_STAT_AREA]
+        colon_black_pixels = stats[label, cv2.CC_STAT_AREA]
 
-        columns_black_pixels_sum = sum(period_columns_histogram) - period_black_pixels
+        columns_black_pixels_sum = sum(colon_columns_histogram) - colon_black_pixels
 
-        if PERIOD_AREA_LOWER_BOUND < area < PERIOD_AREA_UPPER_BOUND:
-            if PERIOD_BOTTOM_BOUNDINGBOX_LOWER_BOUND < bottom < PERIOD_BOTTOM_BOUNDINGBOX_UPPER_BOUND \
-                    and top > PERIOD_TOP_BOUNDINGBOX_LOWER_BOUND \
-                    and columns_black_pixels_sum > BLACK_PIXELS_PERIOD_HISTOGRAM_THRESHOLD:
+        if COLON_AREA_LOWER_BOUND < area < COLON_AREA_UPPER_BOUND:
+            if COLON_BOTTOM_BOUNDINGBOX_LOWER_BOUND < bottom < COLON_BOTTOM_BOUNDINGBOX_UPPER_BOUND \
+                    and top > COLON_TOP_BOUNDINGBOX_LOWER_BOUND \
+                    and columns_black_pixels_sum > BLACK_PIXELS_COLON_HISTOGRAM_THRESHOLD:
                 x, y, width, height = cv2.boundingRect(line_image[:, left:right])
-
 
                 colons_in_line.append((line_left + left, line_top + y, line_left + left + width, line_top + y + height))
 
-    # cv2.imwrite(output_path_image, page_image)
     return colons_in_line
 
 
 def find_long_accents_in_line(page_image, line_left, line_top, line_right, line_bottom):
-    ACCENT_AREA_LOWER_BOUND = 10
-    ACCENT_AREA_UPPER_BOUND = 50
-    ACCENT_BOTTOM_BOUNDINGBOX_LOWER_BOUND = 0
-    ACCENT_BOTTOM_BOUNDINGBOX_UPPER_BOUND = 29
-    ACCENT_TOP_BOUNDINGBOX_LOWER_BOUND = -1
-    ACCENT_TOP_BOUNDINGBOX_UPPER_BOUND = 7
-    BLACK_PIXELS_ACCENT_HISTOGRAM_THRESHOLD = 15
-    ACCENT_WIDTH_BOUNDINGBOX_LOWER_BOUND = 6
-    ACCENT_WIDTH_BOUNDINGBOX_UPPER_BOUND = 15
-    ACCENT_DENSITY_THRESHOLD = 0.0
+    LONG_ACCENT_AREA_LOWER_BOUND = 10
+    LONG_ACCENT_AREA_UPPER_BOUND = 50
+    LONG_ACCENT_BOTTOM_BOUNDINGBOX_LOWER_BOUND = 0
+    LONG_ACCENT_BOTTOM_BOUNDINGBOX_UPPER_BOUND = 29
+    LONG_ACCENT_TOP_BOUNDINGBOX_LOWER_BOUND = -1
+    LONG_ACCENT_TOP_BOUNDINGBOX_UPPER_BOUND = 7
+    BLACK_PIXELS_LONG_ACCENT_HISTOGRAM_THRESHOLD = 15
+    LONG_ACCENT_WIDTH_BOUNDINGBOX_LOWER_BOUND = 6
+    LONG_ACCENT_WIDTH_BOUNDINGBOX_UPPER_BOUND = 15
+    LONG_ACCENT_DENSITY_THRESHOLD = 0.0
 
     long_accents_in_line = []
 
@@ -151,20 +145,19 @@ def find_long_accents_in_line(page_image, line_left, line_top, line_right, line_
         right = left + stats[label, cv2.CC_STAT_WIDTH]
 
         area = (bottom - top) * (right - left)
-        period_columns_histogram = cv2.reduce(line_image[:, left:right], 0, cv2.REDUCE_SUM, dtype=cv2.CV_32F).reshape(
+        long_accent_columns_histogram = cv2.reduce(line_image[:, left:right], 0, cv2.REDUCE_SUM, dtype=cv2.CV_32F).reshape(
             -1) / 255
-        accent_black_pixels = stats[label, cv2.CC_STAT_AREA]
+        long_accent_black_pixels = stats[label, cv2.CC_STAT_AREA]
 
-        columns_black_pixels_sum = sum(period_columns_histogram) - accent_black_pixels
-        accent_bounding_box_density = accent_black_pixels / area
+        columns_black_pixels_sum = sum(long_accent_columns_histogram) - long_accent_black_pixels
+        accent_bounding_box_density = long_accent_black_pixels / area
 
-        if ACCENT_AREA_LOWER_BOUND < area < ACCENT_AREA_UPPER_BOUND:
-            if ACCENT_BOTTOM_BOUNDINGBOX_LOWER_BOUND < bottom < ACCENT_BOTTOM_BOUNDINGBOX_UPPER_BOUND \
-                    and ACCENT_TOP_BOUNDINGBOX_LOWER_BOUND < top < ACCENT_TOP_BOUNDINGBOX_UPPER_BOUND \
-                    and ACCENT_WIDTH_BOUNDINGBOX_LOWER_BOUND < right - left < ACCENT_WIDTH_BOUNDINGBOX_UPPER_BOUND \
-                    and columns_black_pixels_sum > BLACK_PIXELS_ACCENT_HISTOGRAM_THRESHOLD \
-                    and accent_bounding_box_density > ACCENT_DENSITY_THRESHOLD:
-
+        if LONG_ACCENT_AREA_LOWER_BOUND < area < LONG_ACCENT_AREA_UPPER_BOUND:
+            if LONG_ACCENT_BOTTOM_BOUNDINGBOX_LOWER_BOUND < bottom < LONG_ACCENT_BOTTOM_BOUNDINGBOX_UPPER_BOUND \
+                    and LONG_ACCENT_TOP_BOUNDINGBOX_LOWER_BOUND < top < LONG_ACCENT_TOP_BOUNDINGBOX_UPPER_BOUND \
+                    and LONG_ACCENT_WIDTH_BOUNDINGBOX_LOWER_BOUND < right - left < LONG_ACCENT_WIDTH_BOUNDINGBOX_UPPER_BOUND \
+                    and columns_black_pixels_sum > BLACK_PIXELS_LONG_ACCENT_HISTOGRAM_THRESHOLD \
+                    and accent_bounding_box_density > LONG_ACCENT_DENSITY_THRESHOLD:
                 long_accents_in_line.append((line_left + left, line_top + top, line_left + right, line_top + bottom))
 
     # cv2.imwrite(output_path_image, page_image)
@@ -216,10 +209,8 @@ def find_dots_over_i_in_line(page_image, line_left, line_top, line_right, line_b
                     and DOT_WIDTH_BOUNDINGBOX_LOWER_BOUND < right - left < DOT_WIDTH_BOUNDINGBOX_UPPER_BOUND \
                     and columns_black_pixels_sum > BLACK_PIXELS_DOT_HISTOGRAM_THRESHOLD \
                     and dot_bounding_box_density < DOT_DENSITY_UPPER_BOUND:
-
                 dots_over_i_in_line.append((line_left + left, line_top + top, line_left + right, line_top + bottom))
 
-    # cv2.imwrite(output_path_image, page_image)
     return dots_over_i_in_line
 
 
@@ -246,7 +237,6 @@ def draw_punctuation(image_path, output_path):
                               (component[2], component[3]),
                               (190, 0, 255),
                               1)
-
 
             for component in long_accents_in_line:
                 cv2.rectangle(page_image,
@@ -280,7 +270,7 @@ def draw_punctuation(image_path, output_path):
 
 
 def main():
-    image_path = "../dataset/deskewed/genesis/037.png"
+    image_path = "../dataset/deskewed/genesis/025.png"
     output_path = pathlib.Path("../dataset/punctuation/genesis/")
 
     draw_punctuation(image_path, output_path)
